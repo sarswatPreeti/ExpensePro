856e333d8008ff410a34239cc73d15598ddc9c66
sarswatPreeti <saraswatpreeti.developer@gmail.com>
2025-07-20 01:55:56 +0530

    Trigger clean Vercel build

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,4 +1,3 @@
-import React from "react";
 import TopNavbar from "../components/topNavbar";
 import FeatureSection from "../components/featureCard";
 import AuthSection from "../components/authSection";
