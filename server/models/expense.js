137c3891d58b9ef73c76c93a9b6d6d9d7e3af2b0
sarswatPreeti <saraswatpreeti.developer@gmail.com>
2025-08-15 00:44:49 +0530

    feat(expense): update model to support user-specific data

--- server/models/expense.js
+++ server/models/expense.js
@@ -1,12 +1,62 @@
-const { DataTypes } = require("sequelize");
+// const { DataTypes } = require("sequelize");
 
-{/*Sequelize model for an Expense Table*/}
-module.exports = (sequelize) =>
-  sequelize.define("Expense", {
-    title: { type: DataTypes.STRING(100), allowNull: false },
-    amount: { type: DataTypes.FLOAT, allowNull: false },
-    date: { type: DataTypes.DATEONLY, allowNull: false },
-    category: { type: DataTypes.STRING, allowNull: false },
-    description: { type: DataTypes.STRING(255), allowNull: true }, // Optional field
-    invoice: { type: DataTypes.STRING, allowNull: true,} // Optional field
+// {/*Sequelize model for an Expense Table*/}
+// module.exports = (sequelize) =>
+//   sequelize.define("Expense", {
+//     title: { type: DataTypes.STRING(100), allowNull: false },
+//     amount: { type: DataTypes.FLOAT, allowNull: false },
+//     date: { type: DataTypes.DATEONLY, allowNull: false },
+//     category: { type: DataTypes.STRING, allowNull: false },
+//     description: { type: DataTypes.STRING(255), allowNull: true }, // Optional field
+//     invoice: { type: DataTypes.STRING, allowNull: true,} // Optional field
+//   });
+
+module.exports = (sequelize, DataTypes) => {
+  const Expense = sequelize.define("Expense", {
+    title: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        min: 0,
+      },
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    invoice: {
+      type: DataTypes.STRING, // Can store filename or file URL
+      allowNull: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   });
+
+  Expense.associate = (models) => {
+    Expense.belongsTo(models.User, {
+      foreignKey: "userId",
+      onDelete: "CASCADE", // Delete expenses if user is deleted
+    });
+
+    Expense.belongsTo(models.Category, {
+      foreignKey: "categoryId",
+      onDelete: "RESTRICT", // Prevent deleting a category that's in use
+    });
+  };
+
+  return Expense;
+};
