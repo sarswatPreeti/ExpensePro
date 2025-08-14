c037c8c68c36eab14a4a95d9f8510831b45b167b
sarswatPreeti <saraswatpreeti.developer@gmail.com>
2025-08-14 23:57:20 +0530

    feat(database): add User model/configuration to database setup

---
+++ server/config/database.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  dialect: "postgres",
+  logging: isProduction ? false : console.log, // Optional: logs only in dev
+  dialectOptions: isProduction
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : {},
+});
+
+module.exports = sequelize;
