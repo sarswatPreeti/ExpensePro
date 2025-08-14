fd0586c20a46b20963d7b115d17cfe9a5f15f3c6
sarswatPreeti <saraswatpreeti.developer@gmail.com>
2025-08-15 00:45:25 +0530

    chore: update models/index.js to include user-auth changes for Category and Expense

--- server/models/index.js
+++ server/models/index.js
@@ -9,15 +9,26 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "postgres",
-    logging: false,
+    logging: false, // Disable logging in production
   }
 );
 
 const db = {};
+
+// Attach Sequelize core objects
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.Expense = require("./expense")(sequelize);
-db.Category = require("./category")(sequelize, Sequelize);
+// Initialize models
+db.User = require("./user")(sequelize, Sequelize.DataTypes);
+db.Expense = require("./expense")(sequelize, Sequelize.DataTypes);
+db.Category = require("./category")(sequelize, Sequelize.DataTypes);
+
+// Apply associations automatically if defined
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
 
 module.exports = db;
