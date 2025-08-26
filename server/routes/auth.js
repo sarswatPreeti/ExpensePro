4cd596d8f174048912c6feeb6afe7ddb6ee47d97
sarswatPreeti <saraswatpreeti.developer@gmail.com>
2025-08-26 22:55:33 +0530

    fix(auth): update auth routes to improve authentication handling

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,5 +6,6 @@ const authController = require("../controllers/authController");
 router.post("/firebase-login", authController.verifyFirebaseTokenAndIssueJWT);
 router.post("/firebase-signup", authController.verifyFirebaseSignupAndIssueJWT);
 router.post("/forgot-password", authController.forgotPassword);
+router.post("/firebase-verify", authController.verifyFirebaseTokenAndIssueJWT); // For token refresh
 
 module.exports = router;
