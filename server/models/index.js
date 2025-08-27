8c3a63257e91cf108bc763da0e2ad9392624f6db
sarswatPreeti <saraswatpreeti.developer@gmail.com>
2025-08-27 21:42:19 +0530

    chore(server): make database centralized for all

--- server/models/index.js
+++ server/models/index.js
@@ -1,17 +1,7 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: false,
-  }
-);
+// Reuse the centralized, SSL-aware Sequelize instance
+const sequelize = require("../config/database");
 
 const db = {};
 db.Sequelize = Sequelize;
